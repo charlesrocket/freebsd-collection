2aea161f25d35eea7b4c6d1923ef92d6c791bb5b
-k <slowdive@me.com>
2022-03-07 09:34:22 -0800

    Add PSU indicator

--- files/power.sh
+++ files/power.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
-charge=$(hwstat | grep -E 'Cap remain:' | tr -cd '[[:digit:]]')
-ac=$(hwstat | grep 'Time remain:' | sed 's/Time remain://' | tr -d '[:space:]')
+charge="$(hwstat | grep -E 'Cap remain:' | tr -cd '[[:digit:]]')"
+ac="$(hwstat | grep 'Time remain:' | sed 's/Time remain://' | tr -d '[:space:]')"
 
-if [ ${ac} == OnAC ]; then
-    stat="ac"
+if [ "${ac}" == "OnAC" ]
+then
+stat=ac
 fi
 
-echo -e '{"text": "", "alt": "'$stat'", "tooltip": '$charge', "class": "$class", "percentage": '$charge' }'
+case $charge in
+  *[0-9]*)
+    source="battery"
+    ;;
+  *) 
+   source="psu" 
+    ;;
+esac
+
+echo -e '{"text": "", "alt": "'$source'", "tooltip": '$charge', "class": "'$stat'", "percentage": '$charge' }'
 
 exit 0
