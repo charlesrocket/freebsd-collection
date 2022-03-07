8d2191ba8f6a63fd154de2336285e8acc6b39728
-k <slowdive@me.com>
2022-03-07 09:30:02 -0800

    Reformat

--- files/net-if.sh
+++ files/net-if.sh
@@ -2,18 +2,23 @@
 
 ifc=$(ifconfig | pcregrep -M -o '^[^\t:]+:([^\n]|\n\t)*status: active' | egrep -o -m 1 '^[^\t:]+')
 
-if [ ${ifc} == wlan0 ]; then
-	echo ""
-elif [ ${ifc} == eth0 ]; then
-	echo ""
-elif [ ${ifc} == ue0 ]; then
-        echo ""
-elif [ ${ifc} == re0 ]; then
-        echo ""
-elif [ ${ifc} == em0 ]; then
-        echo ""
+if [ "${ifc}" == "wlan0" ]
+then
+echo ""
+elif [ "${ifc}" == "eth0" ]
+then
+echo ""
+elif [ "${ifc}" == "ue0" ]
+then
+echo ""
+elif [ "${ifc}" == "re0" ]
+then
+echo ""
+elif [ "${ifc}" == "em0" ]
+then
+echo ""
 else
-	echo ""
+echo ""
 fi
 
 exit 0
