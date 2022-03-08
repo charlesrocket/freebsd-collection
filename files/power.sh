6967fd1ea80f8ab456ef8ebdef84d6a6e6bd50ff
-k <slowdive@me.com>
2022-03-08 10:25:13 -0800

    Improve `custom-power` module

--- files/power.sh
+++ files/power.sh
@@ -3,20 +3,30 @@
 charge="$(hwstat | grep -E 'Cap remain:' | tr -cd '[[:digit:]]')"
 ac="$(hwstat | grep 'Time remain:' | sed 's/Time remain://' | tr -d '[:space:]')"
 
-if [ "${ac}" == "OnAC" ]
-then
-stat=ac
-fi
+charged=100
+critical=20
 
 case $charge in
   *[0-9]*)
     source="battery"
-    ;;
+     if [ "$charge" -eq "$charged" ]
+     then
+     crw=charged
+     elif [ "$charge" -lt "$critical" ]
+     then
+     crw=critical
+     fi
+     ;;
   *)
-   source="psu"
-    ;;
+     source="psu"
+     ;;
 esac
 
-echo -e '{"text": "", "alt": "'$source'", "tooltip": '$charge', "class": "'$stat'", "percentage": '$charge' }'
+if [ "$ac" == "OnAC" ]
+then
+stat=ac
+fi
+
+echo -e '{"text": "", "alt": "'$source'", "tooltip": '$charge', "class": ["'$stat'", "'$crw'"], "percentage": '$charge' }'
 
 exit 0
