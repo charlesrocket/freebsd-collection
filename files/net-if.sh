aacf7763bc12301500a9afa261fefc44c3596cdd
-k <slowdive@me.com>
2022-03-09 21:25:36 -0800

    Add wireguard check

--- files/net-if.sh
+++ files/net-if.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
 ifc=$(ifconfig | pcregrep -M -o '^[^\t:]+:([^\n]|\n\t)*status: active' | egrep -o -m 1 '^[^\t:]+')
+wg=$(wg show | grep "latest handshake" |  tr -cd '[[:digit:]]')
+
+if [ "${wg}" -ge 0 ]
+then
+vpn="active"
+else
+vpn="inactive"
+fi
 
 if [ "${ifc}" == "wlan0" ]
 then
@@ -20,8 +28,6 @@ if=""
 else
 if=""
 fi
-
-vpn="inactive"
 
 echo -e '{"text": "'$if'", "alt": "'$vpn'", "tooltip": "", "class": "", "percentage": "" }'
 
