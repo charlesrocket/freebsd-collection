33f9bb085bc63712ae7ec75fd4ab0af40f8ba222
-k <slowdive@me.com>
2022-03-21 16:08:52 -0700

    VPN check: Less noise

--- roles/wm/files/net-if.sh
+++ roles/wm/files/net-if.sh
@@ -3,9 +3,12 @@
 ifc=$(ifconfig | pcregrep -M -o '^[^\t:]+:([^\n]|\n\t)*status: active' | egrep -o -m 1 '^[^\t:]+')
 wg=$(wg show | grep "latest handshake" |  tr -cd '[[:digit:]]')
 
+if [ -n "$wg" ]
+then
 if [ "${wg}" -ge 0 ]
 then
 vpn="active"
+fi
 else
 vpn="inactive"
 fi
