08ba46140df06c162ea0dda1775c973ed8c894d7
-k <slowdive@me.com>
2022-04-20 10:32:23 -0700

    Add test script

---
+++ test/idempotence_test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+idempotence_test=$(mktemp)
+
+ansible-galaxy install -r requirements.yml
+
+ansible-playbook freebsd.yml
+ansible-playbook freebsd.yml | tee -a ${idempotence_test}
+tail ${idempotence_test} | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
