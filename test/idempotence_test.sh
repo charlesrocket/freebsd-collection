4e98fd3f45933d0942892307907c28746959120a
-k <slowdive@me.com>
2023-03-05 15:38:40 -0800

    Rename playbook

--- test/idempotence_test.sh
+++ test/idempotence_test.sh
@@ -4,6 +4,6 @@ idempotence_test=$(mktemp)
 
 ansible-galaxy install -r requirements.yml
 
-ansible-playbook freebsd.yml
-ansible-playbook freebsd.yml | tee -a ${idempotence_test}
+ansible-playbook main.yml
+ansible-playbook main.yml | tee -a ${idempotence_test}
 tail ${idempotence_test} | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
