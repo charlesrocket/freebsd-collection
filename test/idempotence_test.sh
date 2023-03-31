a519e86063019331abd18411a26e528cc758a223
-k <slowdive@me.com>
2023-03-31 09:18:45 -0700

    ci: Fix test script

--- test/idempotence_test.sh
+++ test/idempotence_test.sh
@@ -3,7 +3,17 @@
 idempotence_test=$(mktemp)
 
 ansible-galaxy collection build
-ansible-galaxy collection install charlesrocket-freebsd-$(grep version galaxy.yml | awk '{print $2}').tar.gz
-ansible-playbook charlesrocket.freebsd.station -c local -i "localhost," -e "@playbooks/vars/charlesrocket.yml" -e "@test/config.yml"                                              ✭
-ansible-playbook charlesrocket.freebsd.station -c local -i "localhost," -e "@playbooks/vars/charlesrocket.yml" -e "@test/config.yml" | tee -a ${idempotence_test}
-tail ${idempotence_test} | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
+ansible-galaxy collection install \
+    charlesrocket-freebsd-$(grep version galaxy.yml | awk '{print $2}').tar.gz
+
+ansible-playbook charlesrocket.freebsd.station -c \
+    local -i "localhost," -e "@playbooks/vars/charlesrocket.yml" \
+    -e "@test/config.yml"
+
+ansible-playbook charlesrocket.freebsd.station -c \
+    local -i "localhost," -e "@playbooks/vars/charlesrocket.yml" \
+    -e "@test/config.yml" | tee -a ${idempotence_test}
+
+tail ${idempotence_test} | grep -q 'changed=0.*failed=0' \
+    && (echo 'Idempotence test: pass' && exit 0) \
+    || (echo 'Idempotence test: fail' && exit 1)
