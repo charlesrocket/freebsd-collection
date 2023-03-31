cc81d601b69dfdb3dfc7d64a0248a4952575b76d
-k <slowdive@me.com>
2023-03-30 21:37:59 -0700

    test: Add build command

--- test/idempotence_test.sh
+++ test/idempotence_test.sh
@@ -2,6 +2,7 @@
 
 idempotence_test=$(mktemp)
 
+ansible-galaxy collection build
 ansible-galaxy collection install charlesrocket-freebsd-$(grep version galaxy.yml | awk '{print $2}').tar.gz
 ansible-playbook charlesrocket.freebsd.station -c local -i "localhost," -e "@playbooks/vars/charlesrocket.yml" -e "@test/config.yml"                                              ✭
 ansible-playbook charlesrocket.freebsd.station -c local -i "localhost," -e "@playbooks/vars/charlesrocket.yml" -e "@test/config.yml" | tee -a ${idempotence_test}
