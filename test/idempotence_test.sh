c1b7a3141976b3ff1ca40e352b603ce0d048887e
-k <slowdive@me.com>
2023-03-30 21:15:49 -0700

    test: Update test script

--- test/idempotence_test.sh
+++ test/idempotence_test.sh
@@ -2,8 +2,7 @@
 
 idempotence_test=$(mktemp)
 
-ansible-galaxy install -r requirements.yml
-
-ansible-playbook main.yml
-ansible-playbook main.yml | tee -a ${idempotence_test}
+ansible-galaxy collection install charlesrocket-freebsd-$(grep version galaxy.yml | awk '{print $2}').tar.gz
+ansible-playbook charlesrocket.freebsd.station -c local -i "localhost," -e "@playbooks/vars/charlesrocket.yml" -e "@test/config.yml"                                              ✭
+ansible-playbook charlesrocket.freebsd.station -c local -i "localhost," -e "@playbooks/vars/charlesrocket.yml" -e "@test/config.yml" | tee -a ${idempotence_test}
 tail ${idempotence_test} | grep -q 'changed=0.*failed=0' && (echo 'Idempotence test: pass' && exit 0) || (echo 'Idempotence test: fail' && exit 1)
